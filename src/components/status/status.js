b78a05764a5557dcd4d00548dd1abe14cbed4e08
Juan Cardoso da Silva <juancardosodasilva@gmail.com>
2021-09-19 16:05:57 -0300

    Adicionado rota de status no front-end

--- src/components/status/status.js
+++ src/components/status/status.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, } from 'react';
 
 // Import de libs de react.
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,6 +6,7 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import { Link } from "react-router-dom";
 
 import { setId } from '../../id';
+import api from '../../api';
 
 // Import de CSS.
 import './status.css';
@@ -13,18 +14,68 @@ import '../../misc/animations.css';
 import '../../misc/misc.css';
 
 const Status = (props) => {
-  const forms = {
-    id: props.id,
-    form: props.obj
-  };
+  const [id, setIdForm] = useState(props.id);
+  const [status, setStatus] = useState();
 
-  function pushFormEdit(props) {
-    props.history.push(
-      {
-        pathname: "/edit/",
-        state: props.id
+  useEffect(() => {
+    getFormStatus();
+  }, []);
+
+  useEffect(() => {
+    findForm();
+  }, []);
+
+  async function findForm() {
+    let data;
+    let result;
+
+    await api.get(`/formularios/aluno`).then(response => {
+      data = response.data;
+    });
+
+    for (let value of data) {
+      console.log("Pegando form: ",id);
+      console.log(value.cod_formulario);
+      console.log("id === value.cod_formulario ", id === value.cod_formulario);
+      if(id === value.cod_formulario) {
+        result = value;
+        setIdForm(result);
+        console.log("Salvou");
+      } else {
+        result = undefined;
       }
-    );
+    }
+    console.log(id);
+  }
+
+  function handleStatusApro(event) {
+    setStatus(true);
+    setUserFormStatus(event);
+  }
+
+  function handleStatusRepro(event) {
+    setStatus(false);
+    setUserFormStatus(event);
+  }
+
+  async function getFormStatus() {
+    let result;
+    await api.get(`/status/`).then((response) => {
+      result = response.data;
+    });
+    setStatus(result);
+    return result;
+  }
+
+  async function setUserFormStatus(event) {
+    event.preventDefault();
+    try {
+      await api.put('/status/', String(status), id).then(response => {
+        console.log(response);
+      });
+    } catch(e) {
+      console.log(e);
+    }
   }
 
   // Mostra para editar o form.
@@ -69,10 +120,10 @@ const Status = (props) => {
           <div className="card-footer ">
             <div className="btn-group">
               <div className="btn-div-size">
-                <button type="button" className="btn btnSubmitApprove" id="btnSubmit" data-bs-toggle="modal" data-bs-target="#aprovado">Aprovar</button>
+                <button type="button" className="btn btnSubmitApprove" id="btnSubmit" data-bs-toggle="modal" data-bs-target="#aprovado" onClick={handleStatusApro}>Aprovar</button>
               </div>
               <div className="btn-div-size">
-                <button type="button" className="btn btnSubmitClose" id="btnSubmit" data-bs-toggle="modal" data-bs-target="#reprovado">Reprovar</button>
+                <button type="button" className="btn btnSubmitClose" id="btnSubmit" data-bs-toggle="modal" data-bs-target="#reprovado" onClick={handleStatusRepro}>Reprovar</button>
               </div>
             </div>
           </div>
